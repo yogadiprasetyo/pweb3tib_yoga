c956d574b49cf78f916e908230b67e00948bb604
yogadiprasetyo <33107360+yogadiprasetyo@users.noreply.github.com>
2017-10-26 08:36:29 +0700

    Add files via upload

---
+++ CobaGithup.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class GithUp_CobaGithup
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnKirim_Click(sender As Object, e As EventArgs) Handles btnKirim.Click
+        lblHasil.Text = "Nama Anda : " & txtNama.Text & "<br>"
+        lblHasil.Text += "Nim : " & txtNim.Text & "<br>"
+        lblHasil.Text += "Prodi Anda : " & txtProdi.Text
+    End Sub
+End Class
